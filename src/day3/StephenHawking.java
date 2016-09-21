9f069d69c7555a2acb1ec85ee28e5a9f6a23ba3a
wavis421 <wendy.avis@jointheleague.org>
2016-09-21 13:34:33 -0700

    Updates

--- src/day3/StephenHawking.java
+++ src/day3/StephenHawking.java
@@ -2,8 +2,12 @@ package day3;
 
 import java.io.IOException;
 
-public class StephenHawking {
+import sun.applet.Main;
 
+public class StephenHawking {
+public static void main(String[] args) {
+	speak ("What are you doing?");
+}
 	// 1. make a main method and put steps 2, 3 & 4 inside it
 	// 2. ask the user for a sentence
 	// 3. call the speak method below and send it the sentence
